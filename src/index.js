51fa8b4991280623ad984906e3743f0308b11526
Paul O’Shannessy <paul@oshannessy.com>
2016-04-13 10:04:28 -0700

    Support React v15 too
    Fixes #13

--- src/index.js
+++ src/index.js
@@ -18,10 +18,13 @@ function getBackingStorePixelRatio(ctx) {
 }
 
 var getDOMNode;
-if (/^0\.14/.test(React.version)) {
-  getDOMNode = (ref) => ref;
-} else {
+// Super naive semver detection but it's good enough. We support 0.12, 0.13
+// which both have getDOMNode on the ref. 0.14 and 15 make the DOM node the ref.
+var version = React.version.split(/[.-]/);
+if (version[0] === '0' && version[1] === '13' || version[1] === '12') {
   getDOMNode = (ref) => ref.getDOMNode();
+} else {
+  getDOMNode = (ref) => ref;
 }
 
 var QRCode = React.createClass({
