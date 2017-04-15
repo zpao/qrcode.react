f9e98c1b897713b341571e5bc451d30bff324005
Paul O’Shannessy <paul@oshannessy.com>
2017-04-15 16:22:54 -0700

    Remove unnecessary compat code for React < 15

--- src/index.js
+++ src/index.js
@@ -18,16 +18,6 @@ function getBackingStorePixelRatio(ctx) {
   );
 }
 
-var getDOMNode;
-// Super naive semver detection but it's good enough. We support 0.12, 0.13
-// which both have getDOMNode on the ref. 0.14 and 15 make the DOM node the ref.
-var version = React.version.split(/[.-]/);
-if (version[0] === '0' && version[1] === '13' || version[1] === '12') {
-  getDOMNode = (ref) => ref.getDOMNode();
-} else {
-  getDOMNode = (ref) => ref;
-}
-
 class QRCode extends React.Component {
   shouldComponentUpdate(nextProps) {
     return Object.keys(QRCode.propTypes).some((k) => this.props[k] !== nextProps[k]);
@@ -49,7 +39,7 @@ class QRCode extends React.Component {
     qrcode.addData(value);
     qrcode.make();
 
-    var canvas = getDOMNode(this.refs.canvas);
+    var canvas = this.refs.canvas;
 
     var ctx = canvas.getContext('2d');
     var cells = qrcode.modules;
