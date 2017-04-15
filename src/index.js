a470e1beb8599aba7a85d253b94e7185910c7aa1
Dominic Tobias <dominic.tobias@gmail.com>
2017-04-16 00:32:00 +0100

    Use es6 classes as createClass will be removed in v16 (#23)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var React = require('react');
-var createReactClass = require('create-react-class');
 var PropTypes = require('prop-types');
 // qr.js doesn't handle error level of zero (M) so we need to do it right,
 // thus the deep require.
@@ -29,37 +28,20 @@ if (version[0] === '0' && version[1] === '13' || version[1] === '12') {
   getDOMNode = (ref) => ref;
 }
 
-var QRCode = createReactClass({
-  propTypes: {
-    value: PropTypes.string.isRequired,
-    size: PropTypes.number,
-    level: PropTypes.oneOf(['L', 'M', 'Q', 'H']),
-    bgColor: PropTypes.string,
-    fgColor: PropTypes.string,
-  },
-
-  getDefaultProps: function() {
-    return {
-      size: 128,
-      level: 'L',
-      bgColor: '#FFFFFF',
-      fgColor: '#000000',
-    };
-  },
-
-  shouldComponentUpdate: function(nextProps) {
+class QRCode extends React.Component {
+  shouldComponentUpdate(nextProps) {
     return Object.keys(QRCode.propTypes).some((k) => this.props[k] !== nextProps[k]);
-  },
+  }
 
-  componentDidMount: function() {
+  componentDidMount() {
     this.update();
-  },
+  }
 
-  componentDidUpdate: function() {
+  componentDidUpdate() {
     this.update();
-  },
+  }
 
-  update: function() {
+  update() {
     var {value, size, level, bgColor, fgColor} = this.props;
 
     // We'll use type===-1 to force QRCode to automatically pick the best type
@@ -85,9 +67,9 @@ var QRCode = createReactClass({
         ctx.fillRect(Math.round(cdx * tileW), Math.round(rdx * tileH), w, h);
       });
     });
-  },
+  }
 
-  render: function() {
+  render() {
     return (
       <canvas
         style={{height: this.props.size, width: this.props.size}}
@@ -96,7 +78,22 @@ var QRCode = createReactClass({
         ref="canvas"
       />
     );
-  },
-});
+  }
+}
+
+QRCode.propTypes = {
+  value: PropTypes.string.isRequired,
+  size: PropTypes.number,
+  level: PropTypes.oneOf(['L', 'M', 'Q', 'H']),
+  bgColor: PropTypes.string,
+  fgColor: PropTypes.string,
+};
+
+QRCode.defaultProps = {
+  size: 128,
+  level: 'L',
+  bgColor: '#FFFFFF',
+  fgColor: '#000000',
+};
 
 module.exports = QRCode;
