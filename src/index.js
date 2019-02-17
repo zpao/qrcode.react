5bae78cd3e15d6902852050320e050b710f598f1
Paul O’Shannessy <paul@oshannessy.com>
2019-02-17 08:40:38 -0800

    Ensure browser actually supports Path2D
    Edge supports Path2D but not the path string c'tor. It seems to be the
    only browser that doesn't support addPath as well, so we'll use that to
    detect Edge, and fall back to normal canvas operations.
    
    cf. https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/8438884/

--- src/index.js
+++ src/index.js
@@ -116,8 +116,17 @@ function generatePath(modules: [[boolean]], margin: number = 0): string {
 
 // For canvas we're going to switch our drawing mode based on whether or not
 // the environment supports Path2D. We only need the constructor to be
-// supported.
-const SUPPORTS_PATH2D = typeof Path2D === 'function';
+// supported, but Edge doesn't actually support the path (string) type
+// argument. Luckily it also doesn't support the addPath() method. We can
+// treat that as the same thing.
+const SUPPORTS_PATH2D = (function() {
+  try {
+    new Path2D().addPath(new Path2D());
+  } catch (e) {
+    return false;
+  }
+  return true;
+})();
 
 class QRCodeCanvas extends React.PureComponent<QRProps> {
   _canvas: ?HTMLCanvasElement;
