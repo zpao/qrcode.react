134b052a3c08f8140a95cc366bd9090d6c44aa98
Paul O’Shannessy <paul@oshannessy.com>
2021-12-28 14:04:19 -0800

    Additional testing support
    - more coverage of props
    - linting of tests

---
+++ src/__test__/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  // parser: 'babel-eslint',
+  // parserOptions: {
+  //   ecmaVersion: 2016,
+  //   sourceType: 'module',
+  //   ecmaFeatures: {
+  //     jsx: true,
+  //   },
+  // },
+  // env: {
+  //   es6: true,
+  //   node: true,
+  //   browser: true,
+  // },
+  // plugins: ['react'],
+  env: {
+    'jest/globals': true,
+  },
+  plugins: ['jest'],
+  extends: ['plugin:jest/recommended'],
+  // extends: [
+  //   'eslint:recommended',
+  //   'plugin:react/recommended',
+  //   'plugin:flowtype/recommended',
+  //   'plugin:prettier/recommended',
+  //   'prettier',
+  // ],
+  // settings: {
+  //   react: {
+  //     version: '16.2.0',
+  //   },
+  // },
+  // rules: {
+  //   'no-unused-vars': ['error', {ignoreRestSiblings: true}],
+  // },
+};
--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,28 +1,56 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-const QRCode = require('qrcode.react');
+const QRCode = require('../index');
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(
-      <QRCode
-        value={'http://picturesofpeoplescanningqrcodes.tumblr.com/'}
-        size={128}
-        bgColor={'#ffffff'}
-        fgColor={'#000000'}
-        level={'L'}
-        includeMargin={false}
-        renderAs={'svg'}
-        imageSettings={{
-          src: 'https://static.zpao.com/favicon.png',
-          x: null,
-          y: null,
-          height: 24,
-          width: 24,
-          excavate: true,
-        }}
-      />
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+const BASIC_PROPS = {
+  value: 'http://picturesofpeoplescanningqrcodes.tumblr.com/',
+  renderAs: 'svg',
+  size: 128,
+  bgColor: '#ffffff',
+  fgColor: '#000000',
+  level: 'L',
+  includeMargin: false,
+};
+
+const TEST_CONFIGS = [
+  {includeMargin: true},
+  {includeMargin: false},
+  {level: 'L'},
+  {level: 'M'},
+  {level: 'Q'},
+  {level: 'H'},
+  {
+    imageSettings: {
+      src: 'https://static.zpao.com/favicon.png',
+      x: null,
+      y: null,
+      height: 24,
+      width: 24,
+      excavate: true,
+    },
+  },
+  {
+    imageSettings: {
+      src: 'https://static.zpao.com/favicon.png',
+      x: null,
+      y: null,
+      height: 24,
+      width: 24,
+      excavate: false,
+    },
+  },
+];
+
+describe('SVG rendering', () => {
+  test('renders basic SVG correctly', () => {
+    const tree = renderer.create(<QRCode {...BASIC_PROPS} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test.each(TEST_CONFIGS)('renders SVG variation (%o) correctly', (config) => {
+    const tree = renderer
+      .create(<QRCode {...BASIC_PROPS} {...config} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
