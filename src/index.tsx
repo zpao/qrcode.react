1dddeeaf7d27077c30dbc71f9534ca3ad2ff5d4c
Paul O’Shannessy <paul@oshannessy.com>
2022-01-03 22:07:10 -0800

    Replace qr.js with new library, package directly
    Note: the QR Codes generated are not identical. This seems to be due to
    the exact approach taken when encoding strings (leaving encoding to the
    third-party lib again). If there are issues with Kanji or other
    multibyte strings, we'll need to revisit this decision, potentially fork
    this new lib or use it differently.
    
    https://www.nayuki.io/page/qr-code-generator-library

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,15 @@
 import * as React from 'react';
+import qrcodegen from './third-party/qrcodegen';
 
-// qr.js doesn't handle error level of zero (M) so we need to do it right,
-// thus the deep require.
-import QRCodeImpl from 'qr.js/lib/QRCode';
-import ErrorCorrectLevel from 'qr.js/lib/ErrorCorrectLevel';
-// const QRCodeImpl = require('qr.js/lib/QRCode');
-// const ErrorCorrectLevel = require('qr.js/lib/ErrorCorrectLevel');
-
-// TODO: pull this off of the QRCode class type so it matches.
-type Modules = Array<Array<boolean>>;
+type Modules = ReturnType<qrcodegen.QrCode['getModules']>;
 type Excavation = {x: number; y: number; w: number; h: number};
 
-// Convert from UTF-16, forcing the use of byte-mode encoding in our QR Code.
-// This allows us to encode Hanji, Kanji, emoji, etc. Ideally we'd do more
-// detection and not resort to byte-mode if possible, but we're trading off
-// a smaller library for a smaller amount of data we can potentially encode.
-// Based on http://jonisalonen.com/2012/from-utf-16-to-utf-8-in-javascript/
-function convertStr(str: string): string {
-  let out = '';
-  for (let i = 0; i < str.length; i++) {
-    let charcode = str.charCodeAt(i);
-    if (charcode < 0x0080) {
-      out += String.fromCharCode(charcode);
-    } else if (charcode < 0x0800) {
-      out += String.fromCharCode(0xc0 | (charcode >> 6));
-      out += String.fromCharCode(0x80 | (charcode & 0x3f));
-    } else if (charcode < 0xd800 || charcode >= 0xe000) {
-      out += String.fromCharCode(0xe0 | (charcode >> 12));
-      out += String.fromCharCode(0x80 | ((charcode >> 6) & 0x3f));
-      out += String.fromCharCode(0x80 | (charcode & 0x3f));
-    } else {
-      // This is a surrogate pair, so we'll reconsitute the pieces and work
-      // from that
-      i++;
-      charcode =
-        0x10000 + (((charcode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff));
-      out += String.fromCharCode(0xf0 | (charcode >> 18));
-      out += String.fromCharCode(0x80 | ((charcode >> 12) & 0x3f));
-      out += String.fromCharCode(0x80 | ((charcode >> 6) & 0x3f));
-      out += String.fromCharCode(0x80 | (charcode & 0x3f));
-    }
-  }
-  return out;
-}
+const ERROR_LEVEL_MAP: {[index: string]: qrcodegen.QrCode.Ecc} = {
+  L: qrcodegen.QrCode.Ecc.LOW,
+  M: qrcodegen.QrCode.Ecc.MEDIUM,
+  Q: qrcodegen.QrCode.Ecc.QUARTILE,
+  H: qrcodegen.QrCode.Ecc.HIGH,
+};
 
 type QRProps = {
   value: string;
@@ -226,11 +193,6 @@ class QRCodeCanvas extends React.PureComponent<QRProps, {imgLoaded: boolean}> {
     const {value, size, level, bgColor, fgColor, includeMargin, imageSettings} =
       this.props;
 
-    // We'll use type===-1 to force QRCode to automatically pick the best type
-    const qrcode = new QRCodeImpl(-1, ErrorCorrectLevel[level]);
-    qrcode.addData(convertStr(value));
-    qrcode.make();
-
     if (this._canvas != null) {
       const canvas = this._canvas;
 
@@ -239,10 +201,10 @@ class QRCodeCanvas extends React.PureComponent<QRProps, {imgLoaded: boolean}> {
         return;
       }
 
-      let cells = qrcode.modules;
-      if (cells === null) {
-        return;
-      }
+      let cells = qrcodegen.QrCode.encodeText(
+        value,
+        ERROR_LEVEL_MAP[level]
+      ).getModules();
 
       const margin = includeMargin ? MARGIN_SIZE : 0;
       const numCells = cells.length + margin * 2;
@@ -355,15 +317,10 @@ class QRCodeSVG extends React.PureComponent<QRProps> {
       ...otherProps
     } = this.props;
 
-    // We'll use type===-1 to force QRCode to automatically pick the best type
-    const qrcode = new QRCodeImpl(-1, ErrorCorrectLevel[level]);
-    qrcode.addData(convertStr(value));
-    qrcode.make();
-
-    let cells = qrcode.modules;
-    if (cells === null) {
-      return null;
-    }
+    let cells = qrcodegen.QrCode.encodeText(
+      value,
+      ERROR_LEVEL_MAP[level]
+    ).getModules();
 
     const margin = includeMargin ? MARGIN_SIZE : 0;
     const numCells = cells.length + margin * 2;
