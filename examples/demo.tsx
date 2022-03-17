d281957df87a23d23521194f6ec327096efa4793
Paul O’Shannessy <paul@oshannessy.com>
2022-03-17 15:22:36 -0700

    Update example to use hooks

--- examples/demo.tsx
+++ examples/demo.tsx
@@ -1,286 +1,272 @@
 'use strict';
 
 import QRCode from '..';
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-// TODO: live update demo
-class Demo extends React.Component {
-  state = {
-    value: 'http://picturesofpeoplescanningqrcodes.tumblr.com/',
-    size: 128,
-    fgColor: '#000000',
-    bgColor: '#ffffff',
-    level: 'L',
-    renderAs: 'svg',
-    includeMargin: false,
-    includeImage: true,
-    imageH: 24,
-    imageW: 24,
-    imageX: 0,
-    imageY: 0,
-    imageSrc: 'https://static.zpao.com/favicon.png',
-    imageExcavate: true,
-    centerImage: true,
-  };
+function Demo() {
+  const [value, setValue] = useState(
+    'https://picturesofpeoplescanningqrcodes.tumblr.com/'
+  );
+  const [size, setSize] = useState(128);
+  const [fgColor, setFgColor] = useState('#000000');
+  const [bgColor, setBgColor] = useState('#ffffff');
+  const [level, setLevel] = useState('L');
+  const [renderAs, setRenderAs] = useState('svg');
+  const [includeMargin, setIncludeMargin] = useState(false);
+  const [includeImage, setIncludeImage] = useState(true);
+  const [imageH, setImageH] = useState(24);
+  const [imageW, setImageW] = useState(24);
+  const [imageX, setImageX] = useState(0);
+  const [imageY, setImageY] = useState(0);
+  const [imageSrc, setImageSrc] = useState(
+    'https://static.zpao.com/favicon.png'
+  );
+  const [imageExcavate, setImageExcavate] = useState(true);
+  const [centerImage, setCenterImage] = useState(true);
 
-  render() {
-    const imageSettingsCode = this.state.includeImage
-      ? `
+  const imageSettingsCode = includeImage
+    ? `
   imageSettings={{
-    src: "${this.state.imageSrc}",
-    x: ${this.state.centerImage ? 'null' : this.state.imageX},
-    y: ${this.state.centerImage ? 'null' : this.state.imageY},
-    height: ${this.state.imageH},
-    width: ${this.state.imageW},
-    excavate: ${this.state.imageExcavate},
+    src: "${imageSrc}",
+    x: ${centerImage ? 'null' : imageX},
+    y: ${centerImage ? 'null' : imageY},
+    height: ${imageH},
+    width: ${imageW},
+    excavate: ${imageExcavate},
   }}`
-      : '';
-    const code = `<QRCode
-  value={"${this.state.value}"}
-  size={${this.state.size}}
-  bgColor={"${this.state.bgColor}"}
-  fgColor={"${this.state.fgColor}"}
-  level={"${this.state.level}"}
-  includeMargin={${this.state.includeMargin}}
-  renderAs={"${this.state.renderAs}"}${imageSettingsCode}
+    : '';
+  const code = `<QRCode
+  value={"${value}"}
+  size={${size}}
+  bgColor={"${bgColor}"}
+  fgColor={"${fgColor}"}
+  level={"${level}"}
+  includeMargin={${includeMargin}}
+  renderAs={"${renderAs}"}${imageSettingsCode}
 />`;
-    return (
+
+  return (
+    <div>
       <div>
+        <label>
+          Size(px):
+          <br />
+          <input
+            type="number"
+            onChange={(e) => setSize(parseInt(e.target.value, 10) || 0)}
+            value={size}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Background Color:
+          <br />
+          <input
+            type="color"
+            onChange={(e) => setBgColor(e.target.value)}
+            value={bgColor}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Foreground Color:
+          <br />
+          <input
+            type="color"
+            onChange={(e) => setFgColor(e.target.value)}
+            value={fgColor}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Error Level:
+          <br />
+          <select onChange={(e) => setLevel(e.target.value)} value={level}>
+            <option value="L">L</option>
+            <option value="M">M</option>
+            <option value="Q">Q</option>
+            <option value="H">H</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <label>
+          Include Margin:
+          <br />
+          <input
+            type="checkbox"
+            checked={includeMargin}
+            onChange={(e) => setIncludeMargin(e.target.checked)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Render As:
+          <br />
+          <select
+            onChange={(e) => setRenderAs(e.target.value)}
+            value={renderAs}>
+            <option value="svg">SVG</option>
+            <option value="canvas">Canvas</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <label>
+          Value:
+          <br />
+          <textarea
+            rows={6}
+            cols={80}
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Include Image:
+          <br />
+          <input
+            type="checkbox"
+            checked={includeImage}
+            onChange={(e) => setIncludeImage(e.target.checked)}
+          />
+        </label>
+      </div>
+
+      <fieldset disabled={!includeImage}>
+        <legend>Image Settings</legend>
+
         <div>
           <label>
-            Size(px):
+            Source:
+            <br />
+            <input
+              type="text"
+              onChange={(e) => setImageSrc(e.target.value)}
+              value={imageSrc}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Image Width: {imageW}
             <br />
             <input
               type="number"
-              onChange={(e) =>
-                this.setState({size: parseInt(e.target.value, 10) || 0})
-              }
-              value={this.state.size}
+              value={imageW}
+              onChange={(e) => setImageW(parseInt(e.target.value, 10))}
             />
           </label>
         </div>
         <div>
           <label>
-            Background Color:
+            Image Height: {imageH}
             <br />
             <input
-              type="color"
-              onChange={(e) => this.setState({bgColor: e.target.value})}
-              value={this.state.bgColor}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Foreground Color:
-            <br />
-            <input
-              type="color"
-              onChange={(e) => this.setState({fgColor: e.target.value})}
-              value={this.state.fgColor}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Error Level:
-            <br />
-            <select
-              onChange={(e) => this.setState({level: e.target.value})}
-              value={this.state.level}>
-              <option value="L">L</option>
-              <option value="M">M</option>
-              <option value="Q">Q</option>
-              <option value="H">H</option>
-            </select>
-          </label>
-        </div>
-        <div>
-          <label>
-            Include Margin:
-            <br />
-            <input
-              type="checkbox"
-              checked={this.state.includeMargin}
-              onChange={(e) => this.setState({includeMargin: e.target.checked})}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Render As:
-            <br />
-            <select
-              onChange={(e) => this.setState({renderAs: e.target.value})}
-              value={this.state.renderAs}>
-              <option value="svg">SVG</option>
-              <option value="canvas">Canvas</option>
-            </select>
-          </label>
-        </div>
-        <div>
-          <label>
-            Value:
-            <br />
-            <textarea
-              rows={6}
-              cols={80}
-              onChange={(e) => this.setState({value: e.target.value})}
-              value={this.state.value}
+              type="number"
+              value={imageH}
+              onChange={(e) => setImageH(parseInt(e.target.value, 10))}
             />
           </label>
         </div>
 
         <div>
           <label>
-            Include Image:
+            Center Image:
             <br />
             <input
               type="checkbox"
-              checked={this.state.includeImage}
-              onChange={(e) => this.setState({includeImage: e.target.checked})}
+              checked={centerImage}
+              onChange={(e) => setCenterImage(e.target.checked)}
             />
           </label>
         </div>
-
-        <fieldset disabled={!this.state.includeImage}>
+        <fieldset disabled={centerImage}>
           <legend>Image Settings</legend>
-
           <div>
             <label>
-              Source:
+              Image X: {imageX}
               <br />
               <input
-                type="text"
-                onChange={(e) => this.setState({imageSrc: e.target.value})}
-                value={this.state.imageSrc}
+                type="range"
+                min={0}
+                max={size - imageW}
+                value={imageX}
+                onChange={(e) => setImageX(parseInt(e.target.value, 10))}
               />
             </label>
           </div>
           <div>
             <label>
-              Image Width: {this.state.imageW}
+              Image Y: {imageY}
               <br />
               <input
-                type="number"
-                value={this.state.imageW}
-                onChange={(e) =>
-                  this.setState({imageW: parseInt(e.target.value, 10)})
-                }
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Image Height: {this.state.imageH}
-              <br />
-              <input
-                type="number"
-                value={this.state.imageH}
-                onChange={(e) =>
-                  this.setState({imageH: parseInt(e.target.value, 10)})
-                }
-              />
-            </label>
-          </div>
-
-          <div>
-            <label>
-              Center Image:
-              <br />
-              <input
-                type="checkbox"
-                checked={this.state.centerImage}
-                onChange={(e) => this.setState({centerImage: e.target.checked})}
-              />
-            </label>
-          </div>
-          <fieldset disabled={this.state.centerImage}>
-            <legend>Image Settings</legend>
-            <div>
-              <label>
-                Image X: {this.state.imageX}
-                <br />
-                <input
-                  type="range"
-                  min={0}
-                  max={this.state.size - this.state.imageW}
-                  value={this.state.imageX}
-                  onChange={(e) =>
-                    this.setState({imageX: parseInt(e.target.value, 10)})
-                  }
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Image Y: {this.state.imageY}
-                <br />
-                <input
-                  type="range"
-                  min={0}
-                  max={this.state.size - this.state.imageH}
-                  value={this.state.imageY}
-                  onChange={(e) =>
-                    this.setState({imageY: parseInt(e.target.value, 10)})
-                  }
-                />
-              </label>
-            </div>
-          </fieldset>
-          <div>
-            <label>
-              Excavate ("dig" foreground to nearest whole module):
-              <br />
-              <input
-                type="checkbox"
-                checked={this.state.imageExcavate}
-                onChange={(e) =>
-                  this.setState({imageExcavate: e.target.checked})
-                }
+                type="range"
+                min={0}
+                max={size - imageH}
+                value={imageY}
+                onChange={(e) => setImageY(parseInt(e.target.value, 10))}
               />
             </label>
           </div>
         </fieldset>
-
         <div>
           <label>
-            Use it:
+            Excavate ("dig" foreground to nearest whole module):
             <br />
-            <textarea
-              rows={code.split('\n').length}
-              cols={80}
-              readOnly={true}
-              value={code}
+            <input
+              type="checkbox"
+              checked={imageExcavate}
+              onChange={(e) => setImageExcavate(e.target.checked)}
             />
           </label>
         </div>
+      </fieldset>
 
-        <QRCode
-          value={this.state.value}
-          size={this.state.size}
-          fgColor={this.state.fgColor}
-          bgColor={this.state.bgColor}
-          level={this.state.level}
-          renderAs={this.state.renderAs}
-          includeMargin={this.state.includeMargin}
-          imageSettings={
-            this.state.includeImage
-              ? {
-                  src: this.state.imageSrc,
-                  height: this.state.imageH,
-                  width: this.state.imageW,
-                  x: this.state.centerImage ? null : this.state.imageX,
-                  y: this.state.centerImage ? null : this.state.imageY,
-                  excavate: this.state.imageExcavate,
-                }
-              : null
-          }
-        />
+      <div>
+        <label>
+          Use it:
+          <br />
+          <textarea
+            rows={code.split('\n').length}
+            cols={80}
+            readOnly={true}
+            value={code}
+          />
+        </label>
       </div>
-    );
-  }
+
+      <QRCode
+        value={value}
+        size={size}
+        fgColor={fgColor}
+        bgColor={bgColor}
+        level={level}
+        renderAs={renderAs}
+        includeMargin={includeMargin}
+        imageSettings={
+          includeImage
+            ? {
+                src: imageSrc,
+                height: imageH,
+                width: imageW,
+                x: centerImage ? null : imageX,
+                y: centerImage ? null : imageY,
+                excavate: imageExcavate,
+              }
+            : null
+        }
+      />
+    </div>
+  );
 }
 
 ReactDOM.render(<Demo />, document.getElementById('demo'));
