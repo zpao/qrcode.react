8269130fed0dea80a19f9cd557ac6894d8ad1f37
Paul O’Shannessy <paul@oshannessy.com>
2024-02-24 23:13:52 -0800

    Create custom hook to memoize some values
    These are being unnecessarily recalculated on every render. It's small
    in the grand scheme, but at least the calculation of the qrcode state
    could be slightly expensive.

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: ISC
  */
 
-import React, {useRef, useEffect, useState, useCallback} from 'react';
+import React, {useRef, useEffect, useState, useCallback, useMemo} from 'react';
 import type {CSSProperties} from 'react';
 import qrcodegen from './third-party/qrcodegen';
 
@@ -171,21 +171,58 @@ function getMarginSize(includeMargin: boolean, marginSize?: number): number {
   return includeMargin ? SPEC_MARGIN_SIZE : DEFAULT_MARGIN_SIZE;
 }
 
-function makeQRCode({
+function useQRCode({
   value,
   level,
   minVersion,
+  includeMargin,
+  marginSize,
+  imageSettings,
+  size,
 }: {
   value: string;
   level: string;
   minVersion: number;
-}): qrcodegen.QrCode {
-  const segments = qrcodegen.QrSegment.makeSegments(value);
-  return qrcodegen.QrCode.encodeSegments(
-    segments,
-    ERROR_LEVEL_MAP[level],
-    minVersion
-  );
+  includeMargin: boolean;
+  marginSize?: number;
+  imageSettings?: ImageSettings;
+  size: number;
+}) {
+  let qrcode = useMemo(() => {
+    const segments = qrcodegen.QrSegment.makeSegments(value);
+    return qrcodegen.QrCode.encodeSegments(
+      segments,
+      ERROR_LEVEL_MAP[level],
+      minVersion
+    );
+  }, [value, level, minVersion]);
+
+  const {cells, margin, numCells, calculatedImageSettings} = useMemo(() => {
+    let cells = qrcode.getModules();
+
+    const margin = getMarginSize(includeMargin, marginSize);
+    const numCells = cells.length + margin * 2;
+    const calculatedImageSettings = getImageSettings(
+      cells,
+      size,
+      margin,
+      imageSettings
+    );
+    return {
+      cells,
+      margin,
+      numCells,
+      calculatedImageSettings,
+    };
+  }, [qrcode, size, imageSettings, includeMargin, marginSize]);
+
+  return {
+    qrcode,
+    margin,
+    cells,
+    numCells,
+    calculatedImageSettings,
+  };
 }
 
 // For canvas we're going to switch our drawing mode based on whether or not
@@ -242,6 +279,16 @@ const QRCodeCanvas = React.forwardRef(function QRCodeCanvas(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isImgLoaded, setIsImageLoaded] = useState(false);
 
+  const {margin, cells, numCells, calculatedImageSettings} = useQRCode({
+    value,
+    level,
+    minVersion,
+    includeMargin,
+    marginSize,
+    imageSettings,
+    size,
+  });
+
   useEffect(() => {
     // Always update the canvas. It's cheap enough and we want to be correct
     // with the current state.
@@ -253,22 +300,7 @@ const QRCodeCanvas = React.forwardRef(function QRCodeCanvas(
         return;
       }
 
-      let qrcode = makeQRCode({
-        value,
-        level,
-        minVersion,
-      });
-      let cells = qrcode.getModules();
-
-      const margin = getMarginSize(includeMargin, marginSize);
-      const numCells = cells.length + margin * 2;
-      const calculatedImageSettings = getImageSettings(
-        cells,
-        size,
-        margin,
-        imageSettings
-      );
-
+      let cellsToDraw = cells;
       const image = _image.current;
       const haveImageToRender =
         calculatedImageSettings != null &&
@@ -279,7 +311,10 @@ const QRCodeCanvas = React.forwardRef(function QRCodeCanvas(
 
       if (haveImageToRender) {
         if (calculatedImageSettings.excavation != null) {
-          cells = excavateModules(cells, calculatedImageSettings.excavation);
+          cellsToDraw = excavateModules(
+            cells,
+            calculatedImageSettings.excavation
+          );
         }
       }
 
@@ -299,7 +334,7 @@ const QRCodeCanvas = React.forwardRef(function QRCodeCanvas(
       ctx.fillStyle = fgColor;
       if (SUPPORTS_PATH2D) {
         // $FlowFixMe: Path2D c'tor doesn't support args yet.
-        ctx.fill(new Path2D(generatePath(cells, margin)));
+        ctx.fill(new Path2D(generatePath(cellsToDraw, margin)));
       } else {
         cells.forEach(function (row, rdx) {
           row.forEach(function (cell, cdx) {
@@ -381,26 +416,21 @@ const QRCodeSVG = React.forwardRef(function QRCodeSVG(
     ...otherProps
   } = props;
 
-  let qrcode = makeQRCode({
+  const {margin, cells, numCells, calculatedImageSettings} = useQRCode({
     value,
     level,
     minVersion,
-  });
-  let cells = qrcode.getModules();
-
-  const margin = getMarginSize(includeMargin, marginSize);
-  const numCells = cells.length + margin * 2;
-  const calculatedImageSettings = getImageSettings(
-    cells,
+    includeMargin,
+    marginSize,
+    imageSettings,
     size,
-    margin,
-    imageSettings
-  );
+  });
 
+  let cellsToDraw = cells;
   let image = null;
   if (imageSettings != null && calculatedImageSettings != null) {
     if (calculatedImageSettings.excavation != null) {
-      cells = excavateModules(cells, calculatedImageSettings.excavation);
+      cellsToDraw = excavateModules(cells, calculatedImageSettings.excavation);
     }
 
     image = (
@@ -422,7 +452,7 @@ const QRCodeSVG = React.forwardRef(function QRCodeSVG(
   // way faster than DOM ops.
   // For level 1, 441 nodes -> 2
   // For level 40, 31329 -> 2
-  const fgPath = generatePath(cells, margin);
+  const fgPath = generatePath(cellsToDraw, margin);
 
   return (
     <svg
